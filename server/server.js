78c4b557520d08b33b02a5b20e217e3e812a213d
Laasya6429 <laasya6429@gmail.com>
2025-12-18 09:17:15 +0530

    added yaml files

--- server/server.js
+++ server/server.js
@@ -23,5 +23,5 @@ app.use('/api/user', userRouter)
 app.use('/api/owner',ownerRouter)
 app.use('/api/bookings', bookingRouter)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
